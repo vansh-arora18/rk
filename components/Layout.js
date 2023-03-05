722e69927306ceb06b784659cbaf46f5a56d4e89
Vansh Arora <vansharora803@gmail.com>
2023-03-06 01:14:31 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -38,7 +38,7 @@ export default function Layout({ title, children }) {
         <header>
           <nav className="flex h-12 items-center px-4 justify-between shadow-md">
             <Link href="/">
-              <a className="text-lg font-bold">amazona</a>
+              <a className="text-lg font-bold">RK Confectionery</a>
             </Link>
             <div>
               <Link href="/cart">
