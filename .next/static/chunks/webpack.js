5c75b5537030650db0c7d2530a28c7913784039d
vansh arora <vansharora@vanshs-MacBook-Pro.local>
2023-03-31 14:49:51 +0530

    payment page update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "91a77965880dd69d"; }
+/******/ 		__webpack_require__.h = function() { return "14604105dda9741d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
