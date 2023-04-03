9eab635c4c890f676abb62a21bbcee141874f840
vansh arora <vansharora@vanshs-MacBook-Pro.local>
2023-04-03 18:04:14 +0530

    search page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "02eda1f75393fc49"; }
+/******/ 		__webpack_require__.h = function() { return "ec4dc399a0296908"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/search.js
@@ -0,0 +1,165 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+import { toast } from "react-toastify";
+import Layout from "../components/Layout";
+import { Store } from "../utils/Store";
+import { XCircleIcon } from "@heroicons/react/outline";
+import ProductItem from "../components/ProductItem";
+import Product from "../models/Product";
+import db from "../utils/db";
+
+const PAGE_SIZE = 6;
+
+export default function Search(props) {
+  const router = useRouter();
+
+  const {
+    query = "all",
+    sort = "featured",
+    page = 1,
+  } = router.query;
+
+  const { products, countProducts, pages } = props;
+
+  const filterSearch = ({
+    page,
+    sort,
+    min,
+    max,
+    searchQuery,
+  }) => {
+    const { query } = router;
+    if (page) query.page = page;
+    if (searchQuery) query.searchQuery = searchQuery;
+    if (sort) query.sort = sort;
+    if (min) query.min ? query.min : query.min === 0 ? 0 : min;
+    if (max) query.max ? query.max : query.max === 0 ? 0 : max;
+
+    router.push({
+      pathname: router.pathname,
+      query: query,
+    });
+  };
+  const pageHandler = (page) => {
+    filterSearch({ page });
+  };
+  const sortHandler = (e) => {
+    filterSearch({ sort: e.target.value });
+  };
+  const { state, dispatch } = useContext(Store);
+  const addToCartHandler = async (product) => {
+    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      toast.error("Sorry. Product is out of stock");
+      return;
+    }
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
+    router.push("/cart");
+  };
+  return (
+    <Layout title="search">
+      <div className="grid md:grid-cols-4 md:gap-5">
+        <div className="md:col-span-3">
+          <div className="mb-2 flex items-center justify-between border-b-2 pb-2">
+            <div className="flex items-center">
+              {products.length === 0 ? "No" : countProducts} Results: {query}
+              &nbsp;
+            </div>
+            <div>
+              Sort by
+              <select value={sort} onChange={sortHandler}>
+                <option value="featured">Featured</option>
+                <option value="lowest">Price: Low to High</option>
+                <option value="highest">Price: High to Low</option>
+              </select>
+            </div>
+          </div>
+          <div>
+            <div className="grid grid-cols-2 gap-1 md:grid-cols-5 lg:grid-cols-7">
+              {products.map((product) => (
+                <ProductItem
+                  key={product._id}
+                  product={product}
+                  addToCartHandler={addToCartHandler}
+                />
+              ))}
+            </div>
+            <ul className="flex justify-center mt-4">
+              {products.length > 0 &&
+                [...Array(pages).keys()].map((pageNumber) => (
+                  <li key={pageNumber}>
+                    <button
+                      className={`default-button m-2 ${
+                        page == pageNumber + 1 ? "font-bold" : ""
+                      } `}
+                      onClick={() => pageHandler(pageNumber + 1)}
+                    >
+                      {pageNumber + 1}
+                    </button>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ query }) {
+  const pageSize = PAGE_SIZE;
+  const page = query.page ||  1;
+  const sort = query.sort || "";
+  const searchQuery = query.query || "";
+
+  const queryFilter =
+    searchQuery
+      ? {
+          name: {
+            $regex: searchQuery,
+            $options: "i",
+          },
+        }:{}
+  const order =
+    sort === "featured"
+      ? { isFeatured: -1 }
+      : sort === "lowest"
+      ? { price: 1 }
+      : sort === "highest"
+      ? { price: -1 }
+      : sort === "toprated"
+      ? { rating: -1 }
+      : sort === "newest"
+      ? { createdAt: -1 }
+      : { _id: -1 };
+
+  await db.connect();
+  const productDocs = await Product.find(
+    {
+      ...queryFilter,
+    },
+  )
+    .sort(order)
+    .skip(pageSize * (page - 1))
+    .limit(pageSize)
+    .lean();
+
+  const countProducts = await Product.countDocuments({
+    ...queryFilter
+  });
+
+  await db.disconnect();
+  const products = productDocs.map(db.convertDocToObj);
+
+  return {
+    props: {
+      products,
+      countProducts,
+      page,
+      pages: Math.ceil(countProducts / pageSize),
+    },
+  };
+}
